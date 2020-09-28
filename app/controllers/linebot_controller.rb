d0a8adef502b3613fe9d0edd44f6416db53c3bc7
yuki sawabe <sawabe0v0@gmail.com>
2020-09-28 19:51:07 +0900

    modify typo

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -52,7 +52,7 @@ class LinebotController < ApplicationController
             per12to18 = doc.elements[xpath + 'info/rainfallchance/period[3]'].text
             per18to24 = doc.elements[xpath + 'info/rainfallchance/period[4]'].text
             if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
-              push =　"今日は雨が降りそうだから傘があった方が良いよ。\n　6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％"
+              push = "今日は雨が降りそうだから傘があった方が良いよ。\n　6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％"
             else
               push = "今日は雨は降らなさそうだよ。今日も一日頑張るだにゃん！！"
             end
